d929667b5b580090b196eb019c4c8420e30833a9
JesusPastorini <juniorpastorini_@hotmail.com>
2023-01-24 19:13:51 -0300

    exercicios

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop_for/exercicio.js
@@ -0,0 +1,9 @@
+/*Utilize o for para imprimir os elementos da lista groceryList com o console.log():*/
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for(let index = 0; index <groceryList.length; index+=1 ){
+    console.log(groceryList[index]);
+  }
+  let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+  for(let nam of names){
+    console.log(nam)
+  }
