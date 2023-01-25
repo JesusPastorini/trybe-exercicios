399f4e2fa65218c38919da8acf6574a1472bc476
JesusPastorini <juniorpastorini_@hotmail.com>
2023-01-25 18:34:56 -0300

    quadrado de asteristico

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-logica_de_programacao-e-algoritmos/exercicio.js
@@ -0,0 +1,11 @@
+let n=4;
+let cont='';
+    for(let i=0;i<n;i+=1){
+        cont=cont+'*';
+    }
+       for(let j=0;j<n;j+=1){
+            
+            console.log(cont)
+        }
+       
+    
