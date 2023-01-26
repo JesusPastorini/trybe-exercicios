26f4ab5866b1037bc0a9b67f0761f2f1e806d3ab
JesusPastorini <juniorpastorini_@hotmail.com>
2023-01-26 15:58:10 -0300

    primeiro exercicio

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios.js
@@ -0,0 +1,16 @@
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+    for(let i in names){
+        console.log('Olá '+names[i])
+    }
+    let car = {
+        model: 'A3 Sedan',
+        manufacturer: 'Audi',
+        year: 2020
+      };
+      for(let j in car){
+        console.log(car);
+      };    
