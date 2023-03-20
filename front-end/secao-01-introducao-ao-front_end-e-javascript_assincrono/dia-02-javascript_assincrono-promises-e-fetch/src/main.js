4768421518d4406892255349bacfb5285faefd6a
JesusPastorini <juniorpastorini_@hotmail.com>
2023-03-20 01:04:16 -0300

    exercicios com api

---
+++ front-end/secao-01-introducao-ao-front_end-e-javascript_assincrono/dia-02-javascript_assincrono-promises-e-fetch/src/main.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+const urlApi = 'https://superheroapi.com/api/5417167581717334';
+
+const img = document.querySelector('#image');
+const name = document.querySelector('#name');
+const button = document.querySelector('#button');
+
+const randomId = () => Math.floor(Math.random() * 1000);
+
+
