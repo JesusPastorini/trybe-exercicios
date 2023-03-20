1d800e12bf02d1022f2929fb02de16e309e66cb6
JesusPastorini <juniorpastorini_@hotmail.com>
2023-03-20 02:53:10 -0300

    adicionando then

--- front-end/secao-01-introducao-ao-front_end-e-javascript_assincrono/dia-02-javascript_assincrono-promises-e-fetch/src/main.js
+++ front-end/secao-01-introducao-ao-front_end-e-javascript_assincrono/dia-02-javascript_assincrono-promises-e-fetch/src/main.js
@@ -8,4 +8,14 @@ const button = document.querySelector('#button');
 
 const randomId = () => Math.floor(Math.random() * 1000);
 
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+    const id = randomId;
+    fetch(`${urlApi}\${id}`)
+    .then((result) => result.json())
+    .then((data) => {
+        img.src = data.imagem.url;
+        name.innerHTML = data.name;
+    });
 
+});
